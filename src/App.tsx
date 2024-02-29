61de1b0070e2c658375496516c3bff0cc6699187
yoosion030 <yoosioff@gmail.com>
2024-02-29 17:11:12 +0900

    feat: 상태 관련 토스트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,12 @@ import {
   DropResult,
 } from "react-beautiful-dnd";
 import "./global.css";
-import { cn } from "./utils";
+import { useToast } from "./ui/use-toast";
+
+interface ItemsType {
+  id: string;
+  content: string;
+}
 
 export default function App() {
   const [enabled, setEnabled] = useState(false);
@@ -20,51 +25,89 @@ export default function App() {
     };
   }, []);
 
-  const [items, setItems] = useState(
+  // TODO: 실제 AIDT 처럼 연동
+  const [items, setItems] = useState<ItemsType[] | null>(
     [...Array(4)].map((_, i) => ({ id: `${i}${i}${i}`, content: `item-${i}` }))
   );
 
-  const onDragEnd = ({ source, destination }: DropResult) => {
-    if (!destination) return;
+  const { toast } = useToast();
+  const [completed, setCompleted] = useState<boolean>(false);
 
-    // 깊은 복사
-    const _items = JSON.parse(JSON.stringify(items)) as typeof items;
-    // 기존 아이템 뽑아내기
-    const [targetItem] = _items.splice(source.index, 1);
-    // 기존 아이템을 새로운 위치에 삽입하기
-    _items.splice(destination.index, 0, targetItem);
-    // 상태 변경
-    setItems(_items);
+  const onDragEnd = ({ source, destination }: DropResult) => {
+    try {
+      if (!destination) return;
+      const _items = JSON.parse(JSON.stringify(items)) as typeof items;
+      const [targetItem] = _items?.splice(source.index, 1) ?? [];
+      setItems(_items);
+      _items?.splice(destination.index, 0, targetItem);
+
+      toast({
+        title: "변경중....",
+      });
+
+      setInterval(() => {
+        localStorage.setItem("items", JSON.stringify(_items));
+        setCompleted(false);
+        setCompleted(true);
+      }, 1500);
+    } catch (error) {
+      toast({
+        title: "변경 실패",
+        description: "변경이 실패했습니다.",
+        variant: "destructive",
+      });
+    }
   };
+
+  const getItems = () => {
+    const data = JSON.parse(localStorage.getItem("items") as string);
+    setItems(data);
+  };
+
+  useEffect(() => {
+    getItems();
+  }, []);
+
+  useEffect(() => {
+    completed &&
+      toast({
+        title: "변경 완료",
+        description: "변경이 완료되었습니다.",
+      });
+  }, [completed, items]);
 
   if (!enabled) {
     return null;
   }
-
-  console.log(items);
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId="droppable">
-        {(provided) => (
-          <div ref={provided.innerRef} {...provided.droppableProps}>
-            {items.map((item, index) => (
-              <Draggable key={item.id} draggableId={item.id} index={index}>
-                {(provided) => (
-                  <div
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                    className="select-none"
-                  >
-                    {item.content}
-                  </div>
-                )}
-              </Draggable>
-            ))}
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
-    </DragDropContext>
+    <>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="droppable">
+          {(provided, snapshot) => (
+            <div
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              className="divide-y-2 space-y-4 w-20"
+            >
+              {items?.map((item, index) => (
+                <Draggable key={item.id} draggableId={item.id} index={index}>
+                  {(provided) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      {item.content}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+      <div onClick={() => {}}> asd</div>
+    </>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Toaster } from "./ui/toaster";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <>
+    <App /> <Toaster />
+  </>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
